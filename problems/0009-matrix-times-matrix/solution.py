45ab552765d26737b1594b26a6b20391c0d5ae87
ajusufi <62121056+ajusufi@users.noreply.github.com>
2026-09-19 16:47:07 +0000

    Solve: Matrix times Matrix

---
+++ problems/0009-matrix-times-matrix/solution.py
@@ -0,0 +1,13 @@
+def matrixmul(a:list[list[int|float]],
+              b:list[list[int|float]])-> list[list[int|float]]:
+    
+    import numpy as np
+
+    a = np.array(a)
+    b = np.array(b)
+
+    try:
+        return a @ b
+    except:
+        return -1
+
